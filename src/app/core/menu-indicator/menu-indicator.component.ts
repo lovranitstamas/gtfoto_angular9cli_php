12b541b89859115459b37b8a7b025f966ba0f5a8
lovranitstamas <tamas198601@gmail.com>
2020-08-27 19:10:32 +0200

    Modify c. indicator, menu indicator, add main menu effect, modify gt icon

--- src/app/core/menu-indicator/menu-indicator.component.ts
+++ src/app/core/menu-indicator/menu-indicator.component.ts
@@ -1,10 +1,20 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-menu-indicator',
   templateUrl: './menu-indicator.component.html',
   styleUrls: ['./menu-indicator.component.scss']
 })
-export class MenuIndicatorComponent {
+export class MenuIndicatorComponent implements OnInit {
   @Input() title;
+  titleArray: Array<string> = [];
+
+  ngOnInit() {
+    for (const word of this.title) {
+      if (word !== ' ') {
+        this.titleArray.push(word);
+      }
+    }
+  }
+
 }
