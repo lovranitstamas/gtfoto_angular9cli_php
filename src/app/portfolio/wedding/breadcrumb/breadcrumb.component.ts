0d68947e9ef9c9ba1883f9a8422394fc4e7acea3
lovranitstamas <tamas198601@gmail.com>
2020-08-27 22:19:54 +0200

    Modify breadcrumb

--- src/app/portfolio/wedding/breadcrumb/breadcrumb.component.ts
+++ src/app/portfolio/wedding/breadcrumb/breadcrumb.component.ts
@@ -1,11 +1,28 @@
-import {Component, Input} from '@angular/core';
+import {Component, HostListener, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
   styleUrls: ['./breadcrumb.component.scss']
 })
-export class BreadcrumbComponent {
+export class BreadcrumbComponent implements OnInit {
   @Input() category;
   @Input() submenu;
+  @Input() dCategory;
+  @Input() dSubmenu;
+
+  windowScrolled: boolean;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    window.scrollY > 200 ? this.windowScrolled = true : this.windowScrolled = false;
+  }
+
+  getStatus() {
+    return this.windowScrolled ? 0.5 : 1;
+  }
+
+  ngOnInit() {
+    this.windowScrolled = false;
+  }
 }
