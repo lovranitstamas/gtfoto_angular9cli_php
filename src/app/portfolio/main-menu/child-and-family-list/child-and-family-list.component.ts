6b1ce0f952ed17bd91c93f68f4a4ec7e974f975e
lovranitstamas <tamas198601@gmail.com>
2020-11-03 23:47:28 +0100

    Translate child and family list header

--- src/app/portfolio/main-menu/child-and-family-list/child-and-family-list.component.ts
+++ src/app/portfolio/main-menu/child-and-family-list/child-and-family-list.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-child-and-family-list',
@@ -6,5 +7,24 @@ import {Component} from '@angular/core';
   styleUrls: ['./child-and-family-list.component.scss']
 })
 export class ChildAndFamilyListComponent {
+  title = 'GYEREK-ÉS CSALÁDI FOTÓK';
+  subtitle = 'Megörökítem életetek egyik legfontosabb napjának legszebb pillanatait.';
+  indicatorTitle = 'GYEREKEK';
 
+  constructor(private _translateService: TranslateService) {
+    this._translateService.onLangChange.subscribe((newLang) => {
+      switch (newLang.lang) {
+        case 'hu':
+          this.title = 'GYEREK-ÉS CSALÁDI FOTÓK';
+          this.subtitle = 'Megörökítem életetek egyik legfontosabb napjának legszebb pillanatait.';
+          this.indicatorTitle = 'GYEREKEK';
+          break;
+        case 'en':
+          this.title = 'CHILD AND FAMILY PHOTOS';
+          this.subtitle = 'Megörökítem életetek egyik legfontosabb napjának legszebb pillanatait.';
+          this.indicatorTitle = 'KIDS';
+          break;
+      }
+    });
+  }
 }
