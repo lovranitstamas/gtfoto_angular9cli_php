3c40bbf5e241ac68cf72a2c302a6f5a2c023915e
lovranitstamas <tamas198601@gmail.com>
2020-11-04 18:28:44 +0100

    Change the detect of lang switch. Put the language variable in stream.

--- src/app/portfolio/wedding/topics/topics.component.ts
+++ src/app/portfolio/wedding/topics/topics.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {LangService} from '../../../shared/lang.service';
 
 @Component({
   selector: 'app-wedding-submenu',
@@ -6,5 +7,22 @@ import {Component} from '@angular/core';
   styleUrls: ['./topics.component.scss']
 })
 export class TopicsComponent {
+  title;
+  subtitle;
 
+  constructor(private _langService: LangService) {
+    this._langService.lang$.subscribe(
+      lang => {
+        switch (lang) {
+          case 'hu':
+            this.title = 'ESKÜVŐI FOTÓK';
+            this.subtitle = 'Megörökítem életetek egyik legfontosabb napjának legszebb pillanatait.';
+            break;
+          case 'en':
+            this.title = 'WEDDING PHOTOS';
+            this.subtitle = 'I record the nicest moments of the most important day of your life.';
+            break;
+        }
+      });
+  }
 }
